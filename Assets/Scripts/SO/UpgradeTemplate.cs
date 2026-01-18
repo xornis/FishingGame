8e281bb7f26437c9f7dcf21f80c74695496b98df
xornis <xornis.dev@gmail.com>
2026-01-18 20:58:25 +0200

    Set up Result UI Panel

---
+++ Assets/Scripts/SO/UpgradeTemplate.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UpgradeTemplate", menuName = "Scriptable Objects/UpgradeTemplate")]
+public class UpgradeTemplate : ScriptableObject
+{
+    [SerializeField] private ResourceType resourceType;
+    [SerializeField] private int minBasePrice;
+    [SerializeField] private int maxBasePrice;
+    [SerializeField] private int minBonusAmount;
+    [SerializeField] private int maxBonusAmount;
+
+    public UpgradeOffer GenerateOffer()
+    {
+        int bonus = Random.Range(minBonusAmount, maxBonusAmount + 1);
+
+        float quality = (maxBonusAmount == minBonusAmount) ? 1f :
+            (float)(bonus - minBonusAmount) / (maxBonusAmount - minBonusAmount);
+
+        int price = Mathf.RoundToInt(Mathf.Lerp(minBasePrice, maxBasePrice, quality));
+
+        return new UpgradeOffer { type = resourceType, price = price, bonusAmount = bonus };
+    }
+
+    public struct UpgradeOffer
+    {
+        public ResourceType type;
+        public int price;
+        public int bonusAmount;
+    }
+}
