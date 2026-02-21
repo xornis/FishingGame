701f718c862e0f674ae846d03cee0f8a157ec46a
xornis <xornis.dev@gmail.com>
2026-02-21 17:39:26 +0200

    Fixed wrong coins number in shop at start
    - also removed unused UpdateFishCount method

--- Assets/Project/Scripts/UI/UIManager.cs
+++ Assets/Project/Scripts/UI/UIManager.cs
@@ -12,6 +12,7 @@ public class UIManager : MonoBehaviour
 
     [Header("References")]
     [SerializeField] private GameEvents gameEvents;
+    [SerializeField] private ResourceManager resourceManager;
     
     private InputAction escapeAction;
 
@@ -68,6 +69,7 @@ public class UIManager : MonoBehaviour
         gameMenuCanvas.Hide();
         shopCanvas.SetupShop(dayNumber, totalFishCaptured);
         shopCanvas.Show();
+        resourceManager.BroadcastAll();
         gameEvents.SendGameplayPermission(false);
     }
 }
